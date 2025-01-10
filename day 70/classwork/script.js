d269f43d0a59c0027b29a5f4e0b386b6d305085b
Vato2010 <vato92832@gmail.com>
2025-01-10 20:57:21 +0400

    second commit

---
+++ day 70/classwork/script.js
@@ -0,0 +1,9 @@
+// ცვლადების შექმნა
+let firstName = "ვატო";
+let lastName = "ჯიკაევი";
+let age = 25; 
+let city = "თბილისი";
+
+let personalInfo = "მე მქვია " + firstName + " " + lastName + ", ვარ " + age + " წლის და ვცხოვრობ " + city + "-ში.";
+console.log(personalInfo);
+alert(personalInfo);
