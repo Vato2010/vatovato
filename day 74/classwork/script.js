a3fba4fdfb677bc6f20e821786ef39587ec06105
Vato2010 <vato92832@gmail.com>
2025-01-19 22:45:04 +0400

    second commit

---
+++ day 74/classwork/script.js
@@ -0,0 +1,43 @@
+// let num = prompt(('enter num: '))
+// if(num == 1){
+//     console.log('num is one');
+// } else if(num == 2){
+//     console.log('num is two');
+// } else if(num == 3){
+//     console.log('num is three');
+// }else if(num == 4){
+//     console.log('num is four');
+// }else{
+//     console.log('arasworia');
+    
+// }
+// let num =Number(prompt('Enter num:')); 
+
+// switch(num){
+//     case 1:
+//         console.log('num is one');
+//         break;
+//     case 2:
+//         console.log('num is two');
+//         break;
+//     case 3:
+//          console.log('num is threee');
+//          break;
+    
+    
+// }
+
+let choice = Number(prompt('enter your choice'));
+switch(choice) {
+    case 1:
+        console.log('americano');
+        break;
+    case 2:
+        console.log('espresso');
+        break;
+    case 3:
+        console.log('cappuchino');
+        break;
+    default:
+        console.log('invalid');    
+}
