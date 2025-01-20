d34c566b106ac7f089cba16be44897039b4b0d05
Vato2010 <vato92832@gmail.com>
2025-01-20 14:37:24 +0400

    second commit

---
+++ day 74/homework/script.js
@@ -0,0 +1,53 @@
+let choice = Number(prompt('enter your choice'));
+switch(choice) {
+    case 1:
+        console.log('ორშაბათი');
+        break;
+    case 2:
+        console.log('სამშაბათი');
+        break;
+    case 3:
+        console.log('ოთხშაბათი');
+        break;
+    case 4:
+        console.log('ხუთშაბათი');
+        break 
+    case 4:
+        console.log('პარასკევი');
+        break
+    case 5:
+        console.log('შაბათი');
+        break
+    case 6:
+        console.log('კვირა');
+        break
+    default:
+        console.log('ასეთი დღე არარსებობს');
+        break
+        
+        
+        
+           
+}
+
+
+
+let archeva = Number(prompt('enter your choice'));
+switch(archeva) {
+    case 1:
+        console.log('მზიანი');
+        break;
+    case 2:
+        console.log('წვიმიანი');
+        break;
+    case 3:
+        console.log('ქარიანი');
+        break;
+    case 4:
+        console.log('მოღრუბლული');
+        break 
+    default:
+        console.log('უცნობია');
+        break
+}
+        
