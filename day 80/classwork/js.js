90cfc4a83c79fc950e9662879eee599cb9123b93
Vato2010 <vato92832@gmail.com>
2025-02-11 14:17:12 +0400

    second commit

---
+++ day 80/classwork/js.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+while (count < 50) {
+    console.log("GOA");
+    count++;
+}
+
+// let num = 20;
+
+// while (num >= 0) {
+//     console.log(num);
+//     num--;
+// }
+// let num = 1;
+
+// while (num <= 100) {
+//     if (num % 5 === 0) {
+//         console.log(num);
+//     }
+//     num++;
+// }
+let num = 1;
+
+while (num <= 20) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+    num++;
+}
---
+++ day 80/homework/script.js
@@ -0,0 +1,21 @@
+function checkPassword() {
+    const password = "Group 41 is best"; 
+    let attempts = 3;
+
+    while (attempts > 0) {
+        let userInput = prompt("შეიყვანეთ პაროლი:"); 
+
+        if (userInput === password) {
+            alert("თქვენი შეყვანილი პაროლი სწორია");
+            return;
+        } else {
+            attempts--;
+            if (attempts > 0) {
+                alert(`პაროლი არასწორია. დაგრჩათ ${attempts} ცდა.`);
+            }
+        }
+    }
+
+    alert("თქვენ ამოგეწურათ ცდების რაოდენობა");
+}
+checkPassword();
