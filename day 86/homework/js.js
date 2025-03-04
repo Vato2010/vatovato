dec54741ceed9c6796b2d9432f3fae7ec42a5b49
Vato2010 <vato92832@gmail.com>
2025-03-04 12:45:33 +0400

    second commit

---
+++ day 86/homework/js.js
@@ -0,0 +1,19 @@
+// let book = {
+//     title: "1984",
+//     author: "George Orwell",
+//     year: 1949,
+//     genre: "Dystopian",
+//     price: 15.99
+
+// }
+// function createBookSentence(book) {
+//     return `The book "${this.title}" was written by ${book.author} in ${book.year}. It belongs to the ${book.genre} genre and costs $${book.price}.`;
+// }
+
+// console.log(createBookSentence(book));
+
+
+// console.log(book);
+// console.log(book.title);
+
+
