94e35b66b677039cb788fd3b5bb2571b6ea80fe5
Vato2010 <vato92832@gmail.com>
2025-05-24 14:18:21 +0400

    second commit

---
+++ day 107/classwork/js.js
@@ -0,0 +1,41 @@
+// let h1 = document.getElementById('headingone')
+
+// h1.style.color = 'red'
+// h1.style.fontFamily = 'cursive'
+
+// h1.innerHTML = 'wow'
+// console.log(h1);
+
+
+// let arraofp = document.getElementsByClassName('p1')
+
+// for(let i =  0 ; i < arraofp.length ; i++){
+//     arraofp[i].style.color = 'blue'
+//     arraofp[i].style.fontFamily = 'cursive'
+//     arraofp[i].innerHTML = 'wow'
+// }
+
+
+// let btn = document.getElementsByTagName('button')
+// console.log(btn);
+// let h1 = document.querySelector('#headingone')
+// let p = document.querySelector('.p1')
+// let btn = document.querySelector('button')
+// console.log([h1, p, btn]);
+// btn.addEventListener('click', function(){
+//     h1.style.color = 'red'
+//     h1.style.fontFamily = 'cursive'
+//     h1.innerHTML = 'wow'
+//     p.style.color = 'blue'
+//     p.style.fontFamily = 'cursive'
+//     p.innerHTML = 'wow'
+// })
+
+
+
+
+// getelementbyid() ეს არის მეთოდი რომელიც გვაძლევს კონკრეტულ ელემენტს მისი id-ის მეშვეობით
+// getelementsbyclassname() ეს არის მეთოდი რომელიც გვაძლებს ელემენტების მასივს მათი კლასის მეშვეობით
+// getelementsbytagname() ეს არის მეთოდი რომელიც გვაძლებს ელემენტების მასივს მათი ტეგის მეშვეობით
+// queryselector() პოულობს მხოლოდ პირველ ელემენტს, რომელიც შეესაბამება CSS სელექტორს
+// queryselectorall() პოულობს ყველა ელემენტს CSS სელექტორითშ
